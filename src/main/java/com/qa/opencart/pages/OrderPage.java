f034f6e9d6f32829bfc6519fdc03612f118f10a3
KK <your.email@example.com>
2025-05-03 18:31:51 +0530

    added order page

---
+++ src/main/java/com/qa/opencart/pages/OrderPage.java
@@ -0,0 +1,14 @@
+package com.qa.opencart.pages;
+
+import org.openqa.selenium.By;
+
+public class OrderPage {
+	
+	By loc = By.id("order");
+	
+	public void getOrder() {
+		System.out.println("get order");
+	}
+		
+
+}
